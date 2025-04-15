352785614a00ca88c9d71e776438edb630d1c87a
Daniel Zeiberg <d.zeiberg@northeastern.edu>
2025-04-15 17:15:37 -0400

    WIP: add test of embedding sequence; need to add a function to EmbeddingSet that extracts sequence from PDB structure and embeds

--- test/test_data_pipeline.py
+++ test/test_data_pipeline.py
@@ -3,6 +3,7 @@ from functional_residue.data.graphs import get_residue_distance_mat
 from functional_residue.data.embeddings import EmbeddingSet
 import pytest
 import numpy as np
+from Bio.PDB.Polypeptide import protein_letters_3to1_extended
 
 
 def test_fetch_pdb():
@@ -32,6 +33,40 @@ def test_get_residue_distance_mat(**kwargs):
     assert distance_matrix[0, 0] == pytest.approx(0)
 
 
+def test_get_embedding():
+    structure = fetch_alphafold_prediction(
+        "P02185", ".test_data", return_structure=True
+    )
+    chain = structure[0]["A"]  # type: ignore
+    residues = list(chain.get_residues())
+    res_names = [
+        protein_letters_3to1_extended[residue.resname.upper()] for residue in residues
+    ]
+    sequence = "".join(res_names)
+    embedding_set = EmbeddingSet()
+    embedding_out = embedding_set.get_many_embeddings(
+        sequences=[
+            sequence,
+        ],
+        ids=[
+            structure.id,
+        ],
+    )
+    assert embedding_out.shape == (1, 1024)
+    seq = "MVLSEGEWQLVLHVWAKVEADVAGHGQDILIRLFKSHPETLEKFDRFKHLKTEAEMKASEDLKKHGVTVLTALGAILKKKGHHEAELKPLAQSHATKHKIPIKYLEFISEAIIHVLHSRHPGDFGADAQGAMNKALELFRKDIAAKYKELGYQG"
+    embedding_out_2 = embedding_set.get_many_embeddings(
+        sequences=[
+            seq,
+        ],
+        ids=[
+            "P02185",
+        ],
+    )
+    assert embedding_out_2.shape == (1, 1024)
+    assert (embedding_out == embedding_out).all()
+
+
 if __name__ == "__main__":
+    test_get_embedding()
     test_get_residue_distance_mat(processes=4)
     print("all tests passed")
